e03c899f947bd1b1132e88eefea4a062e28e01a2
kietthorne <phantuankiet.dev@gmail.com>
2024-04-04 22:16:34 +0700

    social

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,8 +4,29 @@ import footer_logo from "../Assets/logo_big.png";
 import instagram_icon from "../Assets/instagram_icon.png";
 import pintester_icon from "../Assets/pintester_icon.png";
 import whatsapp_icon from "../Assets/whatsapp_icon.png";
+import zalo_icon from "../Assets/zalo_icon.png";
 
 const Footer = () => {
+  // Function to handle Instagram share
+  const shareInstagram = () => {
+    window.open("https://www.instagram.com/", "_blank");
+  };
+
+  // Function to handle Pinterest share
+  const sharePinterest = () => {
+    window.open("https://www.pinterest.com/", "_blank");
+  };
+
+  // Function to handle WhatsApp share
+  const shareWhatsApp = () => {
+    window.open("https://web.whatsapp.com/", "_blank");
+  };
+
+  // Function to handle Zalo share
+  const shareZalo = () => {
+    window.open("https://zalo.me/", "_blank");
+  };
+
   return (
     <div className="footer">
       <div className="footer-logo">
@@ -21,13 +42,16 @@ const Footer = () => {
       </ul>
       <div className="footer-social-icon">
         <div className="footer-icons-container">
-          <img src={instagram_icon} alt="" />
+          <img src={instagram_icon} onClick={shareInstagram} alt="" />
         </div>
         <div className="footer-icons-container">
-          <img src={pintester_icon} alt="" />
+          <img src={pintester_icon} onClick={sharePinterest} alt="" />
         </div>
         <div className="footer-icons-container">
-          <img src={whatsapp_icon} alt="" />
+          <img src={whatsapp_icon} onClick={shareWhatsApp} alt="" />
+        </div>
+        <div className="footer-icons-container">
+          <img src={zalo_icon} onClick={shareZalo} alt="" />
         </div>
       </div>
       <div className="footer-copyright">
